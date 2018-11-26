9a03c44ce09031d420c599850fb203aea7675fc7
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-11-26 19:14:22 +0100

    [example] refactor example code

--- examples/example.py
+++ examples/example.py
@@ -4,8 +4,8 @@ import numpy as np
 
 from sklearn.neighbors import KDTree
 
-from geo3dfeatures.io import xyz as read_xyz
-from geo3dfeatures.extract import featuregen
+from geo3dfeatures.io import xyz as read_xyz, write_features
+from geo3dfeatures.extract import generate_features
 
 
 SEED = 42
@@ -23,28 +23,18 @@ fpath = str(datapath / fname)
 print(f"read the file {fpath}")
 data = read_xyz(str(fpath))
 
-# all rows, and the third columns
-just_points = data[:, [0, 1, 2]]
-assert just_points.shape[1] == 3
+print(f"generate 3D features")
+g = generate_features(data,
+                      nb_neighbors=neighbors_num,
+                      nb_points=5000,
+                      kdtree_leaf_size=1000)
 
-print("compute the KDTree")
-tree = KDTree(just_points, leaf_size=1_000)
+columns = ['a', 'b', 'radius', 'max_difference', 'std_deviation', 'density',
+           'verticality', 'curvature_change', 'linearity', 'planarity',
+           'scattering', 'omnivariance', 'anisotropy',
+           'eigenentropy', 'eigenvalue_sum',
+           'r', 'g', 'b']
 
-# sample the row
-size = 5_000
-choice = np.random.choice(np.arange(just_points.shape[0]), size=size, replace=False)
-sample = (data[idx] for idx in choice)
-
-g = featuregen(sample, data, tree, neighbors_num)
-# g = featuregen(data)
-
-
-columns = ['lambda1', 'lambda2', 'lambda3', 'a', 'b', 'C', 'L', 'P', 'S', 'mudist', 'r', 'g', 'b']
-
-print("compute and write some geo features")
-with open('features.csv', 'w') as fobj:
-    fobj.write(','.join(columns))
-    fobj.write("\n")
-    for row in g:
-        fobj.write(",".join(str(x) for x in row))
-        fobj.write("\n")
+out_fpath = str(datapath / "features.csv")
+print(f"compute and write some geo features in {out_fpath}")
+write_features(out_fpath, g, columns)
--- geo3dfeatures/io.py
+++ geo3dfeatures/io.py
@@ -21,3 +21,26 @@ def xyz(fpath, names=None, header=True):
     np.array
     """
     return np.loadtxt(fpath, delimiter=' ', skiprows=header)
+
+
+def write_features(fpath, gen, columns, sep=","):
+    """Write the data contained into generator in a .csv file
+
+    Parameters
+    ----------
+    fpath : str
+        Path of the file that must be written on the file system
+    gen : generator
+        Data stored as a generator
+    columns : list
+        Header of the .csv file
+    sep : str
+        Separator between data items used in the .csv file
+
+    """
+    with open(fpath, 'w') as fobj:
+        fobj.write(sep.join(columns))
+        fobj.write("\n")
+        for row in gen:
+            fobj.write(sep.join(str(x) for x in row))
+            fobj.write("\n")
