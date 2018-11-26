6d775f2dc926d545a677fa306d296874d4e42006
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-11-26 12:48:21 +0100

    [io] Manage xyz files with no header

--- geo3dfeatures/io.py
+++ geo3dfeatures/io.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 
-def xyz(fpath, names=None):
+def xyz(fpath, names=None, header=True):
     """Read a .xyz file
 
     This is a ASCII text file with 3D coordinates and some extra columns
@@ -13,9 +13,11 @@ def xyz(fpath, names=None):
     ----------
     fpath : str
     names : list (None by default)
+    header : bool
+        If True, there is a header line that must be skipped
 
     Returns
     -------
     np.array
     """
-    return np.loadtxt(fpath, delimiter=' ', skiprows=1)
+    return np.loadtxt(fpath, delimiter=' ', skiprows=header)
