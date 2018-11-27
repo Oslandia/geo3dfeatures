6fda0aebc569b9812a81555a9ab79bb263aca5c0
Raphaël Delhome <raphael.delhome@oslandia.com>
2018-11-27 17:38:58 +0100

    [example] Add argument parser to test the program easily
    Some additional arguments may be added, the example program may be
    improved as well. As most importantly, unit tests still have to be
    written.

--- examples/example.py
+++ examples/example.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+import argparse
 import numpy as np
 
 from sklearn.neighbors import KDTree
@@ -7,38 +8,53 @@ from sklearn.neighbors import KDTree
 from geo3dfeatures.io import xyz as read_xyz, write_features
 from geo3dfeatures.extract import generate_features
 
-
 SEED = 42
 np.random.seed(SEED)
 
-neighbors_num = 50
+if __name__=='__main__':
 
-_here = Path(__file__).absolute().parent
-datapath = _here / '..' / 'data'
-# fname = "vegetation-segment.xyz"
-# fname = "floor-segment.xyz"
-fname = "scene.xyz"
-fpath = str(datapath / fname)
+    parser = argparse.ArgumentParser(description=("3D point cloud geometric"
+                                                  " feature extraction"))
+    parser.add_argument('-n', '--neighbors',
+                        type=int, default=50,
+                        help="Number of neighbors to consider")
+    parser.add_argument('-o', '--output-file',
+                        default="data/features.csv",
+                        help="Output csv file name")
+    parser.add_argument('-p', '--sample-points',
+                        type=int, default=1000,
+                        help="Number of sample points to evaluate")
+    parser.add_argument('-t', '--kdtree-leafs',
+                        type=int, default=1000,
+                        help="Number of leafs in KD-tree")
+    args = parser.parse_args()
 
-print(f"read the file {fpath}")
-data = read_xyz(str(fpath))
+    _here = Path(__file__).absolute().parent
+    datapath = _here / '..' / 'data'
+    # fname = "vegetation-segment.xyz"
+    # fname = "floor-segment.xyz"
+    fname = "scene.xyz"
+    fpath = str(datapath / fname)
 
-print(f"generate 3D features")
-g = generate_features(data,
-                      nb_neighbors=neighbors_num,
-                      nb_points=5000,
-                      kdtree_leaf_size=1000)
+    print(f"read the file {fpath}")
+    data = read_xyz(str(fpath))
 
-columns = ['a', 'b',
-           'z', 'radius', 'z_range', 'std_deviation', 'density', 'verticality',
-           'curvature_change', 'linearity', 'planarity',
-           'scattering', 'omnivariance', 'anisotropy',
-           'eigenentropy', 'eigenvalue_sum',
-           'radius_2D', 'density_2D',
-           'eigenvalue_sum_2D', 'eigenvalue_ratio_2D',
-           'bin_density', 'bin_z_range', 'bin_z_std',
-           'r', 'g', 'b']
+    print(f"generate 3D features")
+    g = generate_features(data,
+                          nb_neighbors=args.neighbors,
+                          nb_points=args.sample_points,
+                          kdtree_leaf_size=args.kdtree_leafs)
 
-out_fpath = str(datapath / "features.csv")
-print(f"compute and write some geo features in {out_fpath}")
-write_features(out_fpath, g, columns)
+    columns = ['a', 'b',
+               'z', 'radius', 'z_range', 'std_deviation', 'density', 'verticality',
+               'curvature_change', 'linearity', 'planarity',
+               'scattering', 'omnivariance', 'anisotropy',
+               'eigenentropy', 'eigenvalue_sum',
+               'radius_2D', 'density_2D',
+               'eigenvalue_sum_2D', 'eigenvalue_ratio_2D',
+               'bin_density', 'bin_z_range', 'bin_z_std',
+               'r', 'g', 'b']
+
+    out_fpath = args.output_file
+    print(f"compute and write some geo features in {out_fpath}")
+    write_features(out_fpath, g, columns)
