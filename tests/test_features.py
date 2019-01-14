e3d9ba425454bfec6cb53ba43d6bc827a7243d48
Damien Garaud <damien.garaud@oslandia.com>
2019-01-14 21:06:48 +0100

    test: use 'low' and 'high' parameters for np.random.uniform
    It's better and more explicit than `5 + 2 * np.random.uniform(size=size) - 1`.

--- tests/test_features.py
+++ tests/test_features.py
@@ -18,7 +18,7 @@ def line(size=SIZE):
     """Build x,y,z point clouds as a line for 1D features
     """
     data = np.zeros((size, 3))
-    data[:, 0] = 2 * np.random.uniform(size=size) - 1  # x
+    data[:, 0] = np.random.uniform(low=1, high=2, size=size)  # x
     # y close to zero
     data[:, 1] = np.random.normal(loc=0, scale=0.002, size=size)
     # z close to zero
@@ -31,9 +31,11 @@ def plane(size=SIZE):
     """Build x,y,z point clouds as a plane for 2D features
     """
     data = np.zeros((size, 3))
-    data[:, 0] = 20 + 2 * np.random.uniform(size=size) - 1  # x
-    data[:, 1] = 40 + 2 * np.random.uniform(size=size) - 1  # y
-    data[:, 2] = 5 + np.random.normal(loc=0, scale=0.002, size=size)  # z close to zero
+    # data[:, 0] = 20 + 2 * np.random.uniform(size=size) - 1  # x
+    data[:, 0] = np.random.uniform(low=20, high=22, size=size)  # x
+    # data[:, 1] = 40 + 2 * np.random.uniform(size=size) - 1  # y
+    data[:, 1] = np.random.uniform(low=40, high=42, size=size)  # y
+    data[:, 2] = np.random.normal(loc=5, scale=0.002, size=size)  # z with std close to zero
     return data
 
 
@@ -43,9 +45,9 @@ def sphere(size=SIZE):
     """
     maxsize = size * 2
     data = np.zeros((maxsize, 3))
-    data[:, 0] = 2 * np.random.uniform(size=maxsize) - 1  # x
-    data[:, 1] = 2 * np.random.uniform(size=maxsize) - 1  # y
-    data[:, 2] = 2 * np.random.uniform(size=maxsize) - 1  # z
+    data[:, 0] = np.random.uniform(low=-1, high=1, size=maxsize)  # x
+    data[:, 1] = np.random.uniform(low=-1, high=1, size=maxsize)  # y
+    data[:, 2] = np.random.uniform(low=-1, high=1, size=maxsize)  # z
     d = data[:, 0] * data[:, 0] + data[:, 1] * data[:, 1] + data[:, 2] * data[:, 2]
     # keep just point inside the sphere
     return data[d <= 1.0][:size]
@@ -60,7 +62,7 @@ def ztube(size=SIZE):
     data[:, 0] = np.random.normal(loc=5, scale=0.002, size=size)
     # small y variations
     data[:, 1] = np.random.normal(loc=10, scale=0.002, size=size)
-    data[:, 2] = 10 + 4 * np.random.uniform(size=size) - 1  # z
+    data[:, 2] = np.random.uniform(low=10, high=14, size=size)  # z
     return data
 
 
@@ -69,9 +71,9 @@ def wall(size=SIZE):
     """High verticality. Plane projection on (x,y) should look like to a straight line.
     """
     data = np.zeros((size, 3))
-    data[:, 0] = 2 + np.random.uniform(size=size) - 1
+    data[:, 0] = np.random.uniform(low=1, high=2, size=size)
     data[:, 1] = 2 + 0.5 * data[:, 0]
-    data[:, 2] = 10 + 10 * np.random.uniform(size=size) - 1
+    data[:, 2] = np.random.uniform(low=10, high=20, size=size)
     return data
 
 
@@ -83,13 +85,13 @@ def roof(size=SIZE):
     - plane projection on (x,y) looks like a plane (even a square since max-min are the same of x and y)
     """
     data = np.zeros((size, 3))
-    data[:size//2, 0] = 10 + 6 * np.random.uniform(size=size // 2) - 1  # x
-    data[:size//2, 1] = 20 + 3 * np.random.uniform(size=size // 2) - 1  # y
+    data[:size//2, 0] = np.random.uniform(low=10, high=16, size=size // 2)  # x
+    data[:size//2, 1] = np.random.uniform(low=20, high=23, size=size // 2)  # y
     # increase according to y (linearly)
     z0 = 2.
     data[:size//2, 2] = z0 + 1/3 * data[:size//2, 1]
     # translation over y and minus z
-    data[size//2:, 0] = 10 + 6 * np.random.uniform(size=size // 2) - 1
+    data[size//2:, 0] = np.random.uniform(low=10, high=16, size=size // 2)
     # data[size//2:, 0] = data[:size//2, 0]
     data[size//2:, 1] = 3 + data[:size//2, 1]
     zmax = data[:size//2, 2].max()
