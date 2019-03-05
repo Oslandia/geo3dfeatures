7c00969fa5556e4c1c1b9bf6596a75837e482a7b
Raphaël Delhome <raphael.delhome@oslandia.com>
2019-03-05 15:10:58 +0100

    Move profiling results into a dedicated repo

--- time-measurement.sh
+++ time-measurement.sh
@@ -20,9 +20,9 @@ echo "Sizes of neighborhoods: $3"
 echo "Feature sets: $4"
 
 xp_name=$1
-mkdir -p data/$xp_name/profiling
-mkdir -p data/$xp_name/out
-mkdir -p data/$xp_name/timers
+mkdir -p data/profiling/$xp_name/profiling
+mkdir -p data/profiling/$xp_name/out
+mkdir -p data/profiling/$xp_name/timers
 
 for points in $2 # '1000 5000 10000 50000'
 do
@@ -31,8 +31,8 @@ do
 	for feature_set in $4 # 'alphabeta eigenvalues full'
 	do
 	    echo "Run examples/example.py with $points points, $neighbors neighbors and the $feature_set feature set..."
-	    _output_file="data/$xp_name/out/features-$points-$neighbors-$feature_set.csv"
-	    _profiling_file="data/$xp_name/profiling/profiling-$points-$neighbors-$feature_set"
+	    _output_file="data/profiling/$xp_name/out/features-$points-$neighbors-$feature_set.csv"
+	    _profiling_file="data/profiling/$xp_name/profiling/profiling-$points-$neighbors-$feature_set"
 	    python -m cProfile -o $_profiling_file examples/example.py -i data/scene.xyz --output-file $_output_file -p $points -n $neighbors -t 1000 -f $feature_set
 	    echo "**********"
 	done
