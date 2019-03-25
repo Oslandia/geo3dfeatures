cdbb812e8a96c9f24790edab36ce4102c59ca63b
Raphaël Delhome <raphael.delhome@oslandia.com>
2019-03-25 16:27:13 +0100

    fixtures: add missing import for plotting 3D figures

--- geo3dfeatures/fixtures.py
+++ geo3dfeatures/fixtures.py
@@ -8,6 +8,7 @@ from pathlib import Path
 
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from sklearn.decomposition import PCA
 
 from geo3dfeatures import features
